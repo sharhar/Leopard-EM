fix: match scaling in CCGs

src/tt2dtm/backend.py
@@ -208,10 +208,19 @@ def scale_mip(
     """
     num_pixels = torch.tensor(mip.shape[0] * mip.shape[1])
 
-    correlation_sum = correlation_sum / total_correlation_positions
-    correlation_squared_sum = correlation_squared_sum / total_correlation_positions
-    correlation_squared_sum -= correlation_sum**2
+    correlation_sum = correlation_sum * total_correlation_positions
+    correlation_squared_sum = (
+        correlation_squared_sum / total_correlation_positions
+    ) - (correlation_sum**2)
+    correlation_squared_sum = correlation_squared_sum * num_pixels
     correlation_squared_sum = torch.sqrt(torch.clamp(correlation_squared_sum, min=0))
+    correlation_sum = correlation_sum * (num_pixels**0.5)
+
+    # # Convert sum and squared sum to mean and variance in-place
+    # correlation_sum = correlation_sum / total_correlation_positions
+    # correlation_squared_sum = correlation_squared_sum / total_correlation_positions
+    # correlation_squared_sum -= correlation_sum**2
+    # correlation_squared_sum = torch.sqrt(torch.clamp(correlation_squared_sum, min=0))
 
     # Calculate normalized MIP
     mip_scaled = mip - correlation_sum
@@ -222,7 +231,7 @@ def scale_mip(
         out=mip_scaled,
     )
 
-    mip = mip * (num_pixels**0.5)
+    # mip = mip * (num_pixels**0.5)
 
     return mip, mip_scaled
 
@@ -274,9 +283,15 @@ def normalize_template_projection(
     # Subtract the edge pixel mean and calculate variance of small, unpadded projection
     edge_mean = edge_pixels.mean(dim=-1)
     projections -= edge_mean[..., None, None]
-    variance, mean = torch.var_mean(projections, dim=(-1, -2), keepdim=True)
 
-    # Scale the variance such that the larger padded space has variance of 1
+    # # Calculate variance like cisTEM (does not match desired results...)
+    # variance = (projections**2).sum(dim=(-1, -2), keepdim=True) * relative_size - (
+    #     projections.mean(dim=(-1, -2), keepdim=True) * relative_size
+    # ) ** 2
+
+    # Fast calculation of mean/var using Torch + appropriate scaling.
+    # Scale the variance such that the larger padded space has variance of 1.
+    variance, mean = torch.var_mean(projections, dim=(-1, -2), keepdim=True)
     mean += relative_size
     variance *= relative_size
     variance += (1 / npix_padded) * mean**2
@@ -300,7 +315,7 @@ def do_iteration_statistics_updates(
 ) -> None:
     """Helper function for updating maxima and tracked statistics.
 
-    NOTE: the batch dimensions are effectively unraveled since taking the
+    NOTE: The batch dimensions are effectively unraveled since taking the
     maximum over a single batch dimensions is much faster than
     multi-dimensional maxima.
 

src/tt2dtm/utils/pre_processing.py
@@ -38,7 +38,7 @@ def calculate_whitening_filter_template(
         output_shape=output_shape,
         output_rfft=True,
         output_fftshift=False,
-    )[0]
+    )
 
 
 def calculate_ctf_filter_stack(
@@ -120,11 +120,16 @@ def calculate_ctf_filter_stack(
 def do_image_preprocessing(image: torch.Tensor) -> torch.Tensor:
     """Pre-processes the input image before running the algorithm.
 
+    NOTE: Although we want an image with mean zero and variance 1, we do not divide by
+    the number of pixels because the CCG normalization requires that the CCG be divided
+    by the number of elements. This operation is skipped to save computation time during
+    the search.
+
     1. RFFT
     2. Calculate whitening filter
     2. Zero central pixel
-    3. Whitening filter element-wise multiplication
-    4. Zero central pixel
+    3. Calculate whitening filter and do element-wise multiplication.
+    4. Zero central pixel again (superfluous, but following cisTEM)
     5. Normalize (x /= sqrt(sum(abs(x)**2)); pixelwise)
 
     Parameters
@@ -140,17 +145,24 @@ def do_image_preprocessing(image: torch.Tensor) -> torch.Tensor:
     """
     image_dft = torch.fft.rfftn(image)
     image_dft[0, 0] = 0 + 0j
+
     wf_image = whitening_filter(
         image_dft=image_dft,
         rfft=True,
         fftshift=False,
         do_power_spectrum=True,
-    )[0]
+    )
     image_dft *= wf_image
     image_dft[0, 0] = 0 + 0j  # superfluous, but following cisTEM
-    image_dft /= torch.sqrt(
-        torch.sum(torch.abs(image_dft) ** 2, dim=(-1, -2), keepdim=True)
-    )
+
+    # NOTE: Extra indexing happening with squared_sum so that Hermitian pairs are
+    # counted, but we skip the first column of the RFFT which should not be duplicated.
+    squared_image_dft = torch.abs(image_dft) ** 2
+    squared_sum = squared_image_dft.sum() + squared_image_dft[:, 1:].sum()
+    image_dft /= torch.sqrt(squared_sum)
+
+    # NOTE: skip this operation -- see docstring
+    # image_dft *= image.numel()  # Scale to variance 1 in real-space
 
     return image_dft
 
@@ -204,7 +216,10 @@ def calculate_searched_orientations(
     return get_uniform_euler_angles(
         in_plane_step=in_plane_angular_step,
         out_of_plane_step=out_of_plane_angular_step,
-        phi_ranges=torch.tensor([[phi_min, phi_max]]),
-        theta_ranges=torch.tensor([[theta_min, theta_max]]),
-        psi_ranges=torch.tensor([[psi_min, psi_max]]),
-    )[0]
+        phi_min=phi_min,
+        phi_max=phi_max,
+        theta_min=theta_min,
+        theta_max=theta_max,
+        psi_min=psi_min,
+        psi_max=psi_max,
+    )