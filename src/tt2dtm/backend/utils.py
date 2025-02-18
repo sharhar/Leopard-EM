fix: update so variance is approx 1 over orientation/defocus

src/tt2dtm/backend/utils.py
@@ -77,10 +77,12 @@ def normalize_template_projection(
     relative_size = h * w / (H * W)
     mean = torch.mean(projections, dim=(-2, -1), keepdim=True) * relative_size
     mean *= relative_size
+
+    # First term of the variance calculation
     variance = torch.sum((projections - mean) ** 2, dim=(-2, -1), keepdim=True)
-    variance = (h * w) * variance + (H - h) * (W - w) * (mean**2)
-    # variance /= (H * W) * (H - h) * (W - w)  # NOTE: This would be the correct scaling
-    variance /= (H - h) * (W - w)  # NOTE: But we use this for CCG normalization
+    # Add the second term of the variance calculation
+    variance += (H - h) * (W - w) * mean**2
+    variance /= H * W
 
     return projections / torch.sqrt(variance)
 

src/tt2dtm/utils/pre_processing.py
@@ -122,8 +122,12 @@ def do_image_preprocessing(
     squared_sum = squared_image_rfft.sum() + squared_image_rfft[:, 1:].sum()
     image_rfft /= torch.sqrt(squared_sum)
 
-    # Scale to variance 1 in real-space
-    image_rfft *= image_rfft.numel() * 2
+    # NOTE: We add on extra division by sqrt(num_pixels) so the cross-correlograms
+    # are roughly normalized to have mean 0 and variance 1.
+    # We do this here since Fourier transform is linear, and we don't have to multiply
+    # the cross correlation at each iteration. This *will not* make the image
+    # have variance 1.
+    image_rfft /= image_rfft.numel() ** 0.5
 
     return image_rfft
 